bc03f88b25217539af0838ab940b56aaceeaea93
Stefano Carlesso <stefanocarlesso@gmx.com>
2017-05-01 15:40:22 +0200

    Move templates to separate dir.

--- implementation/www/cgi-bin/mina.py
+++
@@ -1,8 +0,0 @@
-import database
-
-database.add_book(
-monography_title = 'Il continente desaparecido è ricomparso',
-author = ['Gianni Miná', 'Eduardo Galeano', 'Ugo Chávez', 'François Houtart'],
-copies_quantity = 1,
-subject = [3]
-)
